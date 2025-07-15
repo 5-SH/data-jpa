bec411d99899ddf7dae1e197830d8735ba1f5b4d
5-SH <llll_lll_lll@naver.com>
2025-07-16 05:28:25 +0900

    data-jpa init

---
+++ src/test/java/study/data_jpa/DataJpaApplicationTest.java
@@ -0,0 +1,14 @@
+package study.data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DataJpaApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
