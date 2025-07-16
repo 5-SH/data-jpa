f9cd80636503dd9c612cfe0ac87bb7f3f99a8806
5-SH <llll_lll_lll@naver.com>
2025-07-17 00:00:24 +0900

    순수 JPA 기반 Member, Team 레포지토리 추가

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -3,7 +3,11 @@ package study.datajpa.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -11,9 +15,26 @@ public class MemberJpaRepository {
     @PersistenceContext
     private EntityManager em;
 
+//    @Transactional
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(em.find(Member.class, id));
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 
     public Member find(Long id) {
